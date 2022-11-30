02dca0622f1b6705d0257f339a1748f852029fcf
Nidhal Boumaiza <nidhalbmz123@gmail.com>
2022-11-30 01:12:19 +0100

    sdfvs

--- routes/songRouter.js
+++ routes/songRouter.js
@@ -8,12 +8,14 @@ router.route("/deleteSong/:id").delete(songController.deleteSong);
 
 router.route("/updateSong/:id").patch(songController.updateSong);
 
-router.route("/findSongByName/").get(songController.findSongByName);
+router.route("/findSongByName").get(songController.findSongByName);
 
-router.route("/findSongByCategory/").get(songController.findSongByCategory);
+router.route("/findSongByCategory").get(songController.findSongByCategory);
 
-router.route("/findSongByArtist/").get(songController.findSongByArtist);
+router.route("/findSongByArtist").get(songController.findSongByArtist);
 
-router.route("/searchBar/").get(songController.searchBar);
+router.route("/searchBar").get(songController.searchBar);
+
+router.route("/todayHits").get(songController.todayHits);
 
 module.exports = router;
